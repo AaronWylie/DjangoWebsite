7f78101e84c823083a1c3f65c093a5e7e4795d8a
Aaron Wylie <aaronwylie6@gmail.com>
2021-11-30 12:38:38 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-plmx76r+xdk@p$m(664!7f!79uy%rr1pcfyh(h05#u*v5@r+fh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://wirebotapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['18.188.146.161', 'www.thewirebot.com']
 
 
 
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
